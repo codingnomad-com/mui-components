72ae5530fc36e7af0a1a2e8d3c87555db44e4e9a
Manuel Bachl <manuel@coding-nomad.com>
2023-02-23 20:22:25 +0800

    build(rollup): fixing rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,42 +1,76 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import url from '@rollup/plugin-url';
-import external from 'rollup-plugin-peer-deps-external';
-import typescript from 'rollup-plugin-typescript2';
+// import commonjs from '@rollup/plugin-commonjs';
+// import resolve from '@rollup/plugin-node-resolve';
+// import typescript from '@rollup/plugin-typescript';
+// import url from '@rollup/plugin-url';
+// import external from 'rollup-plugin-peer-deps-external';
 
-import pkg from './package.json' assert { type: 'json' };
+// import pkg from './package.json' assert { type: 'json' };
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    // {
-    //   file: pkg.main,
-    //   format: 'cjs',
-    //   exports: 'named',
-    //   sourcemap: true,
-    // },
-    {
-      file: pkg.module,
-      format: 'esm',
-      exports: 'named',
-      sourcemap: true,
-    },
-    // {
-    //   format: 'esm',
-    //   sourcemap: true,
-    //   file: pkg.module,
-    //   plugins: [flatDts()],
-    // },
-  ],
-  plugins: [
-    external(),
-    url(),
-    resolve(),
-    typescript({
-      // rollupCommonJSResolveHack: true,
-      useTsconfigDeclarationDir: true,
-      // clean: true,
-    }),
-    commonjs(),
-  ],
-};
+// export default {
+//   input: 'src/index.tsx',
+//   output: [
+//     {
+//       file: pkg.main,
+//       format: 'cjs',
+//       exports: 'named',
+//       sourcemap: true,
+//     },
+//     {
+//       file: pkg.module,
+//       format: 'esm',
+//       exports: 'named',
+//       sourcemap: true,
+//     },
+//   ],
+//   plugins: [
+//     external(),
+//     url(),
+//     resolve(),
+//     typescript({
+//       // rollupCommonJSResolveHack: true,
+//       useTsconfigDeclarationDir: true,
+//       // clean: true,
+//     }),
+//     commonjs(),
+//   ],
+// };
+
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import tsConfig from './tsconfig.json';
+
+const config = [
+  {
+    input: 'index.ts',
+    output: [
+      {
+        file: 'build/index.js',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        sourceMap: tsConfig.compilerOptions.sourceMap,
+      }),
+    ],
+  },
+  {
+    input: 'build/index.d.ts',
+    output: [
+      {
+        file: 'build/index.d.ts',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      dts({
+        compilerOptions: {
+          baseUrl: tsConfig.compilerOptions.baseUrl,
+        },
+      }),
+    ],
+  },
+];
+
+export default config;
