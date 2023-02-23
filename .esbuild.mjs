786755c6dfee14d56a7d704dc874afd768e377cb
Manuel Bachl <manuel@coding-nomad.com>
2023-02-23 22:01:06 +0800

    build(esbuild): removed rollup and configured esbuild again

--- .esbuild.mjs
+++ .esbuild.mjs
@@ -1,22 +1,61 @@
-import { build } from 'esbuild';
-// import esbuldNamedExportsPlugin from 'esbuild-plugin-named-exports';
+import esbuild from 'esbuild';
+import {
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  statSync,
+  writeFileSync,
+} from 'fs';
+import { join } from 'path';
 
-const shared = {
-  bundle: true,
-  entryPoints: ['src/index.ts'],
-  // plugins: [esbuldNamedExportsPlugin],
-  target: 'es6',
-};
+const dist = join(process.cwd(), 'dist');
 
-build({
-  ...shared,
-  outfile: 'dist/index.js',
-});
+if (!existsSync(dist)) {
+  mkdirSync(dist);
+}
 
-build({
-  ...shared,
-  format: 'esm',
-  outfile: 'dist/index.esm.js',
-});
+const entryPoints = readdirSync(join(process.cwd(), 'src'))
+  .filter(
+    (file) =>
+      !file.endsWith('.ts') &&
+      statSync(join(process.cwd(), 'src', file)).isFile(),
+  )
+  .map((file) => `src/${file}`);
 
-//     "build": "yarn build:es && yarn compile:ts",
+// esm output bundles with code splitting
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    // entryPoints,
+    outdir: 'dist/esm',
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    splitting: true,
+    format: 'esm',
+    define: { global: 'window' },
+    target: ['esnext'],
+  })
+  .catch(() => process.exit(1));
+
+// cjs output bundle
+esbuild
+  .build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/cjs/index.cjs.js',
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    platform: 'node',
+    target: ['node16'],
+  })
+  .catch(() => process.exit(1));
+
+// an entry file for cjs at the root of the bundle
+writeFileSync(join(dist, 'index.js'), "export * from './esm/index.js';");
+
+// an entry file for esm at the root of the bundle
+writeFileSync(
+  join(dist, 'index.cjs.js'),
+  "module.exports = require('./cjs/index.cjs.js');",
+);
