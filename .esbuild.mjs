a033b00d9536557520609fae5605c68612e5beb1
Manuel Bachl <manuel@coding-nomad.com>
2023-02-23 21:43:02 +0800

    build(rollup): readded rollup

--- .esbuild.mjs
+++ .esbuild.mjs
@@ -1,11 +1,22 @@
 import { build } from 'esbuild';
+// import esbuldNamedExportsPlugin from 'esbuild-plugin-named-exports';
 
 const shared = {
-  entryPoints: ['src/index.ts'],
   bundle: true,
+  entryPoints: ['src/index.ts'],
+  // plugins: [esbuldNamedExportsPlugin],
+  target: 'es6',
 };
 
 build({
   ...shared,
   outfile: 'dist/index.js',
 });
+
+build({
+  ...shared,
+  format: 'esm',
+  outfile: 'dist/index.esm.js',
+});
+
+//     "build": "yarn build:es && yarn compile:ts",
